2a0740f5a4589e6e8f4acef10844f7629bae0fbd
jsteedle <jsteedle@users.noreply.github.com>
2016-06-19 22:12:44 -0500

    update for signup.php
    reports error

--- assignment-1/signup.php
+++ assignment-1/signup.php
@@ -1,34 +1,39 @@
 <?php
-/* SignUp php - Assignment 1 by Jesse Steedle */
-		
+//* SignUp php - Assignment 1 by Jesse Steedle */
+
+	error_reporting(E_ALL);
+	ini_set('display_errors', TRUE);
+
+	include ('feed.php');
 	//start session
 	session_start();	
 	//get username and password from $_POST
-	$u = $_POST["username"];
+	$username = $_POST["username"];
 	
-	$pw = $_POST["password"];
+	$password = $_POST["password"];
 	// Hash the password
-	$password = password_hash($pw, PASSWORD_DEFAULT); 
+	$password = password_hash($password, PASSWORD_DEFAULT);
+	//echo $password;
 
-	$n = $_POST["name"];
-	$e = $_POST["email"];
-	$d = $_POST["dob"];
-	$g = $_POST["gender"];
-	$q = $_POST["question"];
-	$a = $_POST["answer"];
-	$l = $_POST["location"];
-	$pp = $_POST["profile_pic"];
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$dob = $_POST["dob"];
+	$gender = $_POST["gender"];
+	$question = $_POST["question"];
+	$answer = $_POST["answer"];
+	$location = $_POST["location"];
+	$profile_pic = $_POST["profile_pic"];
 
 	// Scrub User Input
-	$username = sanitizeString($u);
-	$name = sanitizeString($n);
-	$email = sanitizeString($e);
-	$dob = sanitizeString($d);
-	$gender = sanitizeString($g);
-	$question = sanitizeString($q);
-	$answer = sanitizeString($a);
-	$location = sanitizeString($l);
-	$profile_pic = sanitizeString($pp);
+	$username = sanitizeString($username);
+	$name = sanitizeString($name);
+	$email = sanitizeString($email);
+	$dob = sanitizeString($dob);
+	$gender = sanitizeString($gender);
+	$question = sanitizeString($question);
+	$answer = sanitizeString($answer);
+	$location = sanitizeString($location);
+	$profile_pic = sanitizeString($profile_pic);
 
 
 	// Set the database 
@@ -44,12 +49,12 @@
 	}
 
 	// Insert a new user into users table
-	$result_insert = mysqli_query($conn, "INSERT INTO users(Username, Password, Name, email, dob, gender, verification_question, verification_answer, location, profile_pic) VALUES ('$username', $password, '$name', '$email', '$dob', 'gender', 'question', 'answer', 'location', 'profile_pic')");
-	//$result = mysqli_query($conn, "SELECT * FROM users WHERE username='$username' AND password='$password'");
+	$result_insert = mysqli_query($conn, "INSERT INTO users(`Username`, `Password`, `Name`, `email`, `dob`, `gender`, `verification_question`, `verification_answer`, `location`, `profile_pic`) VALUES ('$username', '$password', '$name', '$email', '$dob', '$gender', '$question', '$answer', '$location', '$profile_pic')");
 
 	if($result_insert){
 		//redirect to feed page 
-		header("Location: feed.php");
+		$_SESSION["username"] = $username;
+		header('Location: feed.php');
 	}else{
 		//throw an error	
 		echo "Oops! Something went wrong! Please try again!";
